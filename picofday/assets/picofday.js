504a4b955a82f2765f20827f6291d1ae0c6c9c2d
Lala <lalagg@hotmail.com>
2020-05-09 20:45:49 -0400

    picture of the day api pulled and working

--- picofday/assets/picofday.js
+++ picofday/assets/picofday.js
@@ -1,36 +1,35 @@
+function displayPicOfDay() {
+  var nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
 
-function displayPicOfDay(){
-  var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
-  
   $.ajax({
-    url: url,
-    method: 'GET'
-  }).then(function(response){
-    console.log(response)
+    url: nasaUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    //A div to show the movie
+    var photoDiv = $("<div class='photo-container'>");
+
+    //Elements from the response I want to use on my page with their elements to be displayed
     var copyright = response.copyright;
+    var copyrightParagraph = $("<p>").text(`Copyright: ${copyright}`);
     var date = response.date;
+    var dateParagraph = $("<p>").text(`Date: ${date}`);
     var explanation = response.explanation;
+    var explanationParagraph = $("<p>").text(`Explanation: ${explanation}`);
     var title = response.title;
+    var titleEl = $("<h2>").text(`${title}`);
     var picOfDay = response.hdurl;
+    var picOfDayImg = $("<img>").attr("src", picOfDay);
 
-    var photoDiv = $("<div>", {class: "photo-container",})
+    photoDiv.append(
+      titleEl,
+      copyrightParagraph,
+      dateParagraph,
+      explanationParagraph,
+      picOfDayImg
+    );
+
+    $("#picofday").append(photoDiv);
   });
-
-              // var ratingParagraph = $('<p>').text('Rating: ' + rating)
-              var copyrightParagraph = $("<p>").text(`Copyright: ${copyright}`);
-              var dateParagraph = $("<p>").text(`Date: ${date}`);
-              var explanationParagraph = $("<p>").text(`Explanation: ${explanation}`);
-              var picOfDayImg = $("<img>", {
-                src: picOfDay,
-              });
-  
-              photoDiv.append(
-               copyrightParagraph,
-                dateParagraph,
-                explanationParagraph,
-                picOfDayImg
-              );
-  
-              $("#picofday").append(photoDiv);
-            });
-};
+}
+displayPicOfDay();
