d1cd6a681ba23409907b5a6f691db639fbef489e
Lala <lalagg@hotmail.com>
2020-05-14 00:18:51 -0400

    working on navbar

--- eventcal/assets/eventcal.js
+++ eventcal/assets/eventcal.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   $(".sidenav").sidenav();
 });
 
-<script>
+/* <script>
 var frame = document.getElementById('myFrame');
     frame.onload = function () {
         var body = frame.contentWindow.document.querySelector('body');
@@ -11,4 +11,4 @@ var frame = document.getElementById('myFrame');
         body.style.fontSize = '20px';
         body.style.lineHeight = '20px';
     };
-</script>
+</script> */
