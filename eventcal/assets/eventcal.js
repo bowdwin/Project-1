9ede410f2227580768cc2e42b4912e6d83d99b0b
Lala <lalagg@hotmail.com>
2020-05-13 22:54:38 -0400

    font

--- eventcal/assets/eventcal.js
+++ eventcal/assets/eventcal.js
@@ -2,3 +2,13 @@ $(document).ready(function () {
   $(".dropdown-trigger").dropdown();
   $(".sidenav").sidenav();
 });
+
+<script>
+var frame = document.getElementById('myFrame');
+    frame.onload = function () {
+        var body = frame.contentWindow.document.querySelector('body');
+        body.style.color = 'red';
+        body.style.fontSize = '20px';
+        body.style.lineHeight = '20px';
+    };
+</script>
